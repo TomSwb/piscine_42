e3fe5735f6268eec5941634608db216c1749e3eb
Thomas Schwab <thomasschwab@bitminded.ch>
2026-06-28 00:16:21 +0200

    adding trials on strlcpy

--- archive/c/C02/ex10/ft_strlcpy.c
+++ archive/c/C02/ex10/ft_strlcpy.c
@@ -3,15 +3,26 @@
 
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 {
-	int		i;
-	unsigned int	len;
+	int	len;
 
-	i = 0;
 	len = 0;
 	if (dest > 0)
+	{
+		while( src[len] != '\0')
 		{
-			§
+			if (len == size)
+			{
+				len--;
+				break;
+			}	
+		dest[len] = src[len];
+		len++;
 		}
+	}
+	dest[len] = '\0';
+	while (src[len] != '\0')
+		len++;
+	return (len);
 //	while (src[i] != '\0')
 //	{
 //		dest[i] = src[i];
@@ -26,16 +37,26 @@ unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 //	return (len);
 }
 
-int	main(void)
+void	test(int size)
 {
 	char	src[] = "Hello world!";
-	char	dest[98983];
-	size_t	result;
-	size_t	resultorg;
+	char	dest[13];
+	int	result;
+	int	resultorg;
 
-	result = ft_strlcpy(dest, src, sizeof(dest));
-	printf("%zu\n", result);
-	resultorg = strlcpy(dest, src, sizeof(dest));
-	printf("%zu", result);
+	result = ft_strlcpy(dest, src, size);
+	printf("Mine: copied '%s', into '%s', length '%d\n'", src, dest, result);
+	resultorg = strlcpy(dest, src, size);
+	printf("OG: copied '%s', into '%s', length '%d\n'", src, dest, result);
+}
+
+int	main(void)
+{
+	test(19);
+	test(13);
+	test(10);
+	test(1);
+	test(0);
+	test(4234534532);
 	return (0);
 }
