cac9a37abcb0fc5419978cae65528dc302535c8b
BitMinded <thomasschwab@bitminded.ch>
2026-07-04 23:45:00 +0200

    Update solver.c

--- rush/rush01/ex00/solver.c
+++ rush/rush01/ex00/solver.c
@@ -13,21 +13,40 @@
 
 int	*ft_solver(int *conditions, int *grid)
 {
-	ft_recursive(grid, 0, conditions)
+	int	size;
+
+	size = 16;
+	if (!ft_recursive(grid, 0, conditions, size))
+		return (0);
+	return(1);
 }
 
 
-int	ft_recursive(int *tab, int index, int *conditions)
+int	ft_recursive(int *tab, int index, int *conditions, int size)
 {
-	if (index < 16)
+	int	num;
+
+	if (index == size)
+		return (1);
+	num = 1;
+	while (num <= 4))
 	{
-		ft_check_num(index)
-		ft_recursive(tab, index + 1, conditions),	
+		if (ft_check_num(num, index, grid, conditions)
+		{
+			grid[index] = num;
+			if ft_recursive(grid, index + 1, conditions, size);
+				return (1);
+			grid[index] = 0;
+		}
+	num++;
 	}
+	return (0);
 }
 
-int	ft_check_num(int index)
+int	ft_check_num(int num, int index, int *grid, int *conditions)
 {
+	
+
 	ft_validate(num)
 }
 
