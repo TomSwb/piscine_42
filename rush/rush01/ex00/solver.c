b1e744bf998feedf5eaad4268cd9f94ba39cca26
BitMinded <thomasschwab@bitminded.ch>
2026-07-05 00:07:20 +0200

    Refactor solver.c by removing unused code
    Removed unused functions and cleaned up the solver logic.

--- rush/rush01/ex00/solver.c
+++ rush/rush01/ex00/solver.c
@@ -45,46 +45,26 @@ int	ft_recursive(int *tab, int index, int *conditions, int size)
 
 int	ft_check_num(int num, int index, int *grid, int *conditions)
 {
+	int	row;
+	int	cln;
+	int c;
+	int r;
 	
-
-	ft_validate(num)
-}
-
-int	ft_validate(int num)
-{
-
-}
-
-
-
-
-char	*ft_solver(int *clean)
-{
-	int	lin;
-	int	cln
-	char	tab[4][4];
-	int	i;
-	
-	lin = 0;
-	cln = 0;
-	i = 0;
-	while (i < 15 && tab[lin])
+	row = index / 4;
+	cln = index % 4;
+	c = 0;
+	r = 0;
+	while(c < 4)
 	{
-		while (tab[lin][cln])
-		{
-			if (clean[i] == 4)
-				tab[lin][cln] = '1';
-			else if (clean[i] == 3)
-				tab[lin][cln] = num[n];
-			else if (clean[i] == 2)
-				tab[lin][cln] = num[n];
-			else 
-				tab[lin][cln] = '4';
-			cln++;
-			i++;
-		}
-		cln = 0;
-		lin++;
+		if (grid[row * 4 + c] == num;
+			return (0);
+		c++
 	}
+	while(r < 4)
+	{
+		if (grid[r * 4 + cln] == num;
+			return (0);
+		r++
+	}
+	return (1);
 }
-
