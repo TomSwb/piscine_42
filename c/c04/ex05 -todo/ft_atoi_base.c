e0d2c62bcf65adfe467ba5ca8d828225fa19494b
BitMinded <thomasschwab@bitminded.ch>
2026-07-07 19:15:47 +0200

    Add files via upload

---
+++ c/c04/ex05 -todo/ft_atoi_base.c
@@ -0,0 +1,140 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                         ::::::::           */
+/*   ft_atoi_base.c                                      :+:    :+:           */
+/*                                                      +:+                   */
+/*   By: tschwab <marvin@42.fr>                        +#+                    */
+/*                                                    +#+                     */
+/*   Created: 2026/07/01 17:30:18 by tschwab        #+#    #+#                */
+/*   Updated: 2026/07/01 17:49:52 by tschwab        ########   odam.nl        */
+/*                                                                            */
+/* ************************************************************************** */
+/*
+#include <stdio.h>
+*/
+int		ft_atoi_base(char *str, char *base);
+int		ft_check_base(char *base);
+char	*ft_strcheck(char *str, char *base);
+int		ft_cvrt(char *str, char *base, int baselen);
+/*
+int	main(void)
+{
+	char	nl;
+
+	nl = '\n';
+	ft_atoi_base("    -45vsd535", "01v236789");
+	write(1, &nl, 1);
+	ft_atoi_base("    ---458vsd535", "0123456789abcdef");
+	write(1, &nl, 1);
+	ft_atoi_base("    -+-458vsd535", "poneyvif");
+	write(1, &nl, 1);
+	ft_atoi_base("    ---458vsd535", "01");
+	write(1, &nl, 1);
+	ft_atoi_base("    -+--458vsd535", "01223456779");
+	write(1, &nl, 1);
+	ft_atoi_base("    ---458vsd535", "012456789a+bcdef");
+	write(1, &nl, 1);
+	ft_atoi_base("    --+-458vsd535", "pone-yvif");
+	write(1, &nl, 1);
+	ft_atoi_base("    ---458vsd535", "5");
+	write(1, &nl, 1);
+	return (0);
+}
+*/
+int	ft_atoi_base(char *str, char *base)
+{
+	int	baselen;
+	int	result;
+	int	i;
+	int	j;
+
+	i = 0;
+	result = 0;
+	baselen = ft_check_base(base);
+	printf("%d", baselen);
+	if (baselen <= 1)
+		return (result);
+	j = 1;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			j = -j;
+		i++;
+	}
+	str = (ft_strcheck(&str[i], base));
+	result = (ft_cvrt(str, base, baselen));
+	return (result * j);
+}
+
+int	ft_check_base(char *base)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (base[i] != '\0')
+	{
+		if ((base[i] == '-') || (base[i] == '+')
+			|| (base[i] >= 9 && base[i] <= 13)
+			|| base[i] == 32 || base[i] == 127)
+			return (0);
+		i++;
+	}
+	i = 0;
+	while (base[i + 1] != '\0')
+	{
+		j = i + 1;
+		while (base[i] != base[j] && base[j] != '\0')
+			j++;
+		if (base[i] == base[j])
+			return (0);
+		i++;
+	}
+	return (i + 1);
+}
+
+char	*ft_strcheck(char *str, char *base)
+{
+	int	i;
+	int	j;
+	int	flag;
+	char	*cleanstr;
+
+	i = 0;
+	flag = 0;
+	cleanstr = 0;
+	while (flag != 1 || str[i] != '\0')
+	{
+		j = 0;
+		while (base[j] != '\0' && flag != 1)
+		{
+			if (base[i] != str[j])
+				flag = 1;
+			j++;
+		}
+		cleanstr[i] = str[i];
+		i++;
+	}
+	return (cleanstr);
+}
+
+int	ft_cvrt(char *str, char *base, int baselen)
+{
+	int	i;
+	int	result;
+	int	j;
+
+	i = 0;
+	result = 0;
+	j = 0;
+	while (str[i] != '\0')
+	{
+		while (str[i] != base[j])
+			j++;
+		result = result * baselen + (str[i] - baselen);
+		i++;
+	}
+	return (result);
+}
