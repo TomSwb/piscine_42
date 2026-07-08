bc0e3d4aafde5e53a1ec0ba2dc0ae189392907e3
BitMinded <thomasschwab@bitminded.ch>
2026-07-08 09:15:30 +0200

    Update ft_sort_params.c

--- c/c06/ex03 -todo/ft_sort_params.c
+++ c/c06/ex03 -todo/ft_sort_params.c
@@ -12,31 +12,30 @@
 
 #include <unistd.h>
 
-void	ft_short_params(int ac, char **arv);
 int	ft_strcmp(char *s1, char *s2);
-void	ft_swap(char *a, char *b);
 void	ft_putstr(char **arr);
 
 int	main(int argc, char **argv)
 {
-	ft_short_params(argc, argv);
-	return (0);
-}
-
-void	ft_short_params(int ac, char **arv)
-{
-	int	i;
+	int		i;
+	char	*temp;
 	
 	i = 1;
-	while (i < ac - 1)
+	while (i < argc - 1)
 	{
-		if (ft_strcmp(arv[i], arv[i + 1]) > 0)
-			ft_swap(arv[i], arv[i + 1]);
-		i++;
+		if (ft_strcmp(argv[i], argv[i + 1]) > 0)
+		{	
+			temp = argv[i];
+			argv[i] = argv[i + 1];
+			argv[i + 1] = temp;
+			i = 1;
+		}
+		else
+			i++;
 	}
-	ft_putstr(arv);
+	ft_putstr(argv);
+	return (0);
 }
-
 
 int	ft_strcmp(char *s1, char *s2)
 {
@@ -46,15 +45,6 @@ int	ft_strcmp(char *s1, char *s2)
 	while (s1[i] && s1[i] == s2[i])	
 		i++;
 	return (s1[i] - s2[i]);
-}
-
-void	ft_swap(char *a, char *b)
-{
-	char	*temp;
-
-	temp = a;
-	a = b;
-	b = temp;
 }
 
 void	ft_putstr(char **arr)
