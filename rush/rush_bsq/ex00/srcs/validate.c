7920655dad3cd612aa4797b54bd1b1ee0cbb1e9b
BitMinded <thomasschwab@bitminded.ch>
2026-07-14 23:17:49 +0200

    Create validate.c

---
+++ rush/rush_bsq/ex00/srcs/validate.c
@@ -0,0 +1,42 @@
+int validate_map(t_map *map)
+{
+    if (validate_lines_length(map))
+        return 1;
+    if (validate_characters(map))
+        return 1;
+    return 0;
+}
+
+int validate_lines_length(t_map *map)
+{
+    int i;
+    int len;
+    len = ft_strlen(map->grid[0]);
+    i = 1;
+    while (i < map->rows)
+    {
+        if (ft_strlen(map->grid[i]) != len)
+            return 1;
+        i++;
+    }
+    return 0;
+}
+int validate_characters(t_map *map)
+{
+    int i;
+    int j;
+    i = 0;
+    while (i < map->rows)
+    {
+        j = 0;
+        while (j < map->cols)
+        {
+            char c = map->grid[i][j];
+            if (c != map->empty && c != map->obstacle)
+                return 1;
+            j++;
+        }
+        i++;
+    }
+    return 0;
+}
